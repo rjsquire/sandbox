a7cc8589f26315085d8baef73ecc246598c7ca39
Robert Squire <rjsquire@gmail.com>
2024-01-11 09:07:23 -0500

    prime_count comes from a cli arg

--- rust/primerator/src/main.rs
+++ rust/primerator/src/main.rs
@@ -1,7 +1,26 @@
-use std::io;
+// use std::io;
+use std::env;
 
 fn main() {
-    let prime_count: i64 = get_prime_count();
+    
+    let args: Vec<String> = env::args().collect();
+    let arg = match args.get(1) {
+        Some(val) => val,
+        None => {
+            println!("Usage: primerator <num>");
+            std::process::exit(1);
+        }
+    };
+
+    let prime_count: i64 = match arg.parse::<i64>() {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Unable to parse number from argument: {}", e);
+            return;
+        }
+    };
+
+    // let prime_count: i64 = get_prime_count();
     let mut primes: Vec<i64> = Vec::new();
     primes.push(2);
     let mut to_test: i64 = 3;
@@ -16,25 +35,25 @@ fn main() {
     println!("{:?}", primes);
 }
 
-fn get_prime_count() -> i64 {
-    println!("How many primes do you want!");
-    let mut prime_count = String::new();
-    loop {    
-        println!("Please input an integer number of primes you want:");
+// fn get_prime_count() -> i64 {
+//     println!("How many primes do you want!");
+//     let mut prime_count = String::new();
+//     loop {    
+//         println!("Please input an integer number of primes you want:");
 
-        io::stdin()
-            .read_line(&mut prime_count)
-            .expect("Failed to read line");
+//         io::stdin()
+//             .read_line(&mut prime_count)
+//             .expect("Failed to read line");
 
-        let prime_count: i64 = match prime_count.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+//         let prime_count: i64 = match prime_count.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue,
+//         };
         
-        println!("You entered: {prime_count}");
-        return prime_count;
-    }
-}
+//         println!("You entered: {prime_count}");
+//         return prime_count;
+//     }
+// }
 
 fn is_prime(x: i64) -> bool {
     // let mut keep_going: bool = true;
@@ -42,7 +61,7 @@ fn is_prime(x: i64) -> bool {
     let _limit: i64 = f64::sqrt(x as f64) as i64 + 1;
     let mut check: i64 = 2;
     // println!("{x} passed to is_prime");
-    while check < _limit {
+    while check <= _limit {
         // println!("Currently checking if {x} % {check} == 0");
         // println!("{:?}", x % check);
         if x % check == 0 || check > _limit {
